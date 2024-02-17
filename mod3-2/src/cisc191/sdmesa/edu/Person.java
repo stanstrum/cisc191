e4228e718b12ca72e6b1c7259110873b2f1d7608
Stan Strum <stanleystrum@gmail.com>
2024-02-16 19:50:34 -0800

    M3-2: add comments to Person

--- mod3-2/src/cisc191/sdmesa/edu/Person.java
+++ mod3-2/src/cisc191/sdmesa/edu/Person.java
@@ -1,28 +1,66 @@
 package cisc191.sdmesa.edu;
 
 /**
+ * Represents a person by their basic personal information
+ *
  * @author Stan Strum
  */
 public class Person {
+  /**
+   * The person's name
+   */
   private String name;
+
+  /**
+   * The person's phone number
+   */
   private String phoneNumber;
+
+  /**
+   * The person's zip code
+   */
   private String zipCode;
 
+  /**
+   * Basic constructor
+   *
+   * @param name name of person
+   * @param phoneNumber phone number of person
+   * @param zipCode zip code of person
+   */
   public Person(String name, String phoneNumber, String zipCode) {
     this.name = name;
     this.phoneNumber = phoneNumber;
     this.zipCode = zipCode;
   }
 
+  /**
+   * @return {@link Person#name}
+   */
   public String getName() {
     return this.name;
   }
 
+  /**
+   * Sets {@link Person#name}
+   *
+   * @param name new name
+   */
   public void setName(String name) {
     this.name = name;
   }
 
+  /**
+   * @return a String representation of this person's basic information
+   */
   public String toString() {
-    return String.format("%s %s %s", this.name, this.phoneNumber, this.zipCode);
+    // e.g. "Alex Keaton 619-555-1212 92111"
+
+    return String.format(
+      "%s %s %s",
+      this.name,
+      this.phoneNumber,
+      this.zipCode
+    );
   }
 }
