0aa241608b5c404fd6017ebc42214d81f6d18d18
Stan Strum <stanleystrum@gmail.com>
2024-02-16 19:03:38 -0800

    M3-2: impl CreditCardCharge methods -- all tests pass now

--- mod3-2/src/cisc191/sdmesa/edu/CreditCardCharge.java
+++ mod3-2/src/cisc191/sdmesa/edu/CreditCardCharge.java
@@ -4,7 +4,32 @@ package cisc191.sdmesa.edu;
  * @author Stan Strum
  */
 public class CreditCardCharge {
-  public CreditCardCharge(int arg0, int arg1, int arg2, Boat arg3, String arg4, CreditCard arg5) {
-    throw new UnsupportedOperationException("Not implemented");
+  private int year;
+  private int month;
+  private int day;
+  private Boat boat;
+  private String company;
+  private CreditCard card;
+
+  public CreditCardCharge(int year, int month, int day, Boat boat, String company, CreditCard card) {
+    this.year = year;
+    this.month = month;
+    this.day = day;
+    this.boat = boat;
+    this.company = company;
+    this.card = card;
+  }
+
+  public String toString() {
+    // e.g. "2020/8/14 4444111122223333 12/24 123 Alex Keaton BoatCo SpeedyBoat $19995"
+
+    return String.format(
+      "%d/%d/%d %s %s %s $%d",
+      this.year, this.month, this.day,
+      this.card.toString(),
+      this.company,
+      this.boat.getMake(),
+      this.boat.getPrice()
+    );
   }
 }
