3558024448de9dc7fb95a2aa7738fa5843035a43
Stan Strum <stanleystrum@gmail.com>
2024-02-16 19:00:19 -0800

    M3-2: impl CreditCard methods

--- mod3-2/src/cisc191/sdmesa/edu/CreditCard.java
+++ mod3-2/src/cisc191/sdmesa/edu/CreditCard.java
@@ -4,7 +4,20 @@ package cisc191.sdmesa.edu;
  * @author Stan Strum
  */
 public class CreditCard {
-  public CreditCard(String arg0, String arg1, String arg2, Person arg3) {
-    throw new UnsupportedOperationException("Not implemented");
+  private String accountNumber;
+  private String expirationDate;
+  private String cvv;
+  private Person holder;
+
+  public CreditCard(String accountNumber, String expirationDate, String cvv, Person holder) {
+    this.accountNumber = accountNumber;
+    this.expirationDate = expirationDate;
+    this.cvv = cvv;
+    this.holder = holder;
+  }
+
+  public String toString() {
+    // e.g. "4444111122223333 12/24 123 Alex Keaton"
+    return String.format("%s %s %s %s", this.accountNumber, this.expirationDate, this.cvv, this.holder.getName());
   }
 }
