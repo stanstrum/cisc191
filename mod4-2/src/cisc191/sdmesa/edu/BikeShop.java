4bba02ad768d1d5ba948648b57e993a289522f28
Stan Strum <stanleystrum@gmail.com>
2024-02-19 17:42:53 -0800

    M4-2: impl BikeShop

--- mod4-2/src/cisc191/sdmesa/edu/BikeShop.java
+++ mod4-2/src/cisc191/sdmesa/edu/BikeShop.java
@@ -1,14 +1,25 @@
 package cisc191.sdmesa.edu;
 
+import java.util.ArrayList;
+
 /**
  * @author Stan Strum {@literal <sstrum@student.sdccd.edu>}
  */
 public class BikeShop {
-	public BikeShop(String string) {
-		throw new UnsupportedOperationException("Not implemented");
+	private final String name;
+
+	private ArrayList<Cycle> inventory = new ArrayList<>();
+
+	public BikeShop(String name) {
+		this.name = name;
 	}
 
-  public void addToInventory(Cycle arg0) {
-		throw new UnsupportedOperationException("Not implemented");
+  public void addToInventory(Cycle cycle) {
+		this.inventory.add(cycle);
   }
+
+	@Override
+	public String toString() {
+		return String.format("BikeShop: %s, inventory: %s", this.name, this.inventory.toString());
+	}
 }
